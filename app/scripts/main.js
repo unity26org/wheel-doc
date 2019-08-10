bbc105fc40e631ef7144dae92d5070b35e698358
Adilson Chacon <adilsonchacon@gmail.com>
2019-08-10 12:06:14 +0000

    install and usage almost done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,3 @@
-console.log('\'Allo \'Allo!');
-
 // Uncomment to enable Bootstrap tooltips
 // https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
 // $(function () { $('[data-toggle="tooltip"]').tooltip(); });
